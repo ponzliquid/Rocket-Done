14fc483de4635158c9a3d3a7ea5cdd463b354fd0
ponz.sierra <fjmtttak@gmail.com>
2017-09-16 15:19:12 +0900

    爆発するようにしたStage2.sceneを追加

---
+++ Assets/Scripts/Stage2/ObstacleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour {
+
+	public GameObject ExploadObj;
+
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag != "Goal") {
+			Destroy (c.gameObject);
+			Instantiate (ExploadObj, c.gameObject.transform.position, Quaternion.identity);
+			Destroy (this.gameObject);
+		}
+	}
+
+}
